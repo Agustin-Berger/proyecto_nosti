79fb3045dbd6c9632220facd48e1d6fbaa4fa78a
Agustin Berger <aguberger07@gmail.com>
2025-03-20 19:37:35 -0300

    venta update

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 
 // Importar y definir los modelos
 const UsuarioModel = require("../back/models/Usuario")(sequelize);
-const VentaModel = require("../back/models/venta")(sequelize);
+const VentaModel = require("../back/models/Venta")(sequelize);
 
 // Definir relaciones
 // Relación muchos a muchos entre Usuario y Venta
